d581e0f233aa4ac2b00cde11633d43d639a2da82
Seadon Francis Pinto <seadonfrank@gmail.com>
2025-09-10 16:49:50 +0000

    Replace with Scan to verify  (_for element p_)

--- src/components/verify-tab.tsx
+++ src/components/verify-tab.tsx
@@ -38,7 +38,7 @@ export default function VerifyTab() {
                       <QrCode className="h-6 w-6" />
                     </div>
                     <div>
-                      <p className="font-semibold">Scan to Prove</p>
+                      <p className="font-semibold">Scan to Verify</p>
                       <p className="text-sm text-muted-foreground">
                         Provide instant zero knowledge proofs
                       </p>
